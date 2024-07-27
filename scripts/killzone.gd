98f143862bed776bc04b3ac919a28ce9537d11d5
Shehab Mustafa Zaher <112824273+Shehab-Zaher@users.noreply.github.com>
2024-07-27 16:24:28 +0300

    Add files via upload

---
+++ scripts/killzone.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var timer = $Timer
+
+func _on_body_entered(body):
+	print("YOU DIED !")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
+	timer.start()
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
---
+++ scripts/player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# get the input direction : -1 , 0 , 1	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+		
+	# pLAY ANIMATION
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("IDLE")
+		else:
+			animated_sprite.play("RUN")
+	else:
+		animated_sprite.play("JUMP")		
+	
+		
+		# Apply movement
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
